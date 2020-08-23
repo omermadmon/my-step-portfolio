acd364a5528209bfe5d8b728d329be791bc87304
omermadmon <omermadmon@gmail.com>
2020-08-23 08:43:42 +0000

    use servlet to display headline and text with the CSS style, and add project name to pom.xml

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -20,13 +20,15 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/** Servlet that returns some example content. TODO: modify this file to handle comments data */
+/** Servlet that returns some example content.*/
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     response.setContentType("text/html;");
-    response.getWriter().println("<h1>Hello world!</h1>");
+    response.getWriter().println("<link rel=\"stylesheet\" href=\"style.css\">");
+    response.getWriter().println("<h1>Servlet Example</h1>");
+    response.getWriter().println("<p>This is my first Servlet usage :)</p>");
   }
 }

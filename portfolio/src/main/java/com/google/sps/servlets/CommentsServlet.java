e84ff228cd1c5fce42a8df250aafed79677b6869
omermadmon <omermadmon@gmail.com>
2020-08-24 06:31:38 +0000

    remove unused import statements.

--- portfolio/src/main/java/com/google/sps/servlets/CommentsServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/CommentsServlet.java
@@ -19,8 +19,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.HashMap;
 import com.google.gson.Gson;
 

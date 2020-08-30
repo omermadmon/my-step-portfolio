a4e219872e1a5c91b943b039f7722e40b52711e0
omermadmon <omermadmon@gmail.com>
2020-08-30 15:06:35 +0000

    create achievement class for representing my FM achievements.

---
+++ portfolio/src/main/java/com/google/sps/data/Achievement.java
@@ -0,0 +1,34 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.sps.data;
+
+/** Class responsible for representing my FM achievements. */
+public final class Achievement {
+
+  private final long id;
+  private final String title;
+  private final String club;
+  private final String geo;
+  private final String text;
+
+  public Achievement(long id, String title, String club, 
+                 String geo, String text) {
+    this.id = id;
+    this.title = title;
+    this.club = club;
+    this.geo = geo;
+    this.text = text;
+  }
+}

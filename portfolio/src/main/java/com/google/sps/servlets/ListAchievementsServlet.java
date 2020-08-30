7f22dfc4951984340c56a4aa0b7fee2ff657a00b
omermadmon <omermadmon@gmail.com>
2020-08-30 15:13:09 +0000

    Create a Servlet responsible for listing achievements to be displayed on achievements page.

---
+++ portfolio/src/main/java/com/google/sps/servlets/ListAchievementsServlet.java
@@ -0,0 +1,69 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.sps.servlets;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.SortDirection;
+import com.google.appengine.api.datastore.FetchOptions;
+import com.google.gson.Gson;
+import com.google.sps.data.Achievement;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/** Servlet responsible for listing achievements to be displayed on achievements page. */
+@WebServlet("/list-achievements")
+public class ListAchievementsServlet extends HttpServlet {
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    
+    // Create query for retrieving all achievements.
+    Query query = new Query("Achievement");
+
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    PreparedQuery results = datastore.prepare(query);
+
+    // Store all achievements in a list.
+    List<Achievement> achievements = new ArrayList<>();
+    for (Entity entity : results.asIterable(FetchOptions.Builder)) {
+      long id = entity.getKey().getId();
+      String title = (String) entity.getProperty("title");
+      String club = (String) entity.getProperty("club");
+      String geo = (String) entity.getProperty("geo");
+      String text = (String) entity.getProperty("text");
+
+      Achievement achievement = new Achievement(id, title, club,
+                                    geo, text);
+      achievements.add(achievement);
+    }
+
+    // Transform comments list to JSON string.
+    Gson gson = new Gson();
+    String json = gson.toJson(achievements);
+
+    // Send the JSON as the response
+    response.setContentType("application/json;");
+    response.getWriter().println(json);
+  }
+}

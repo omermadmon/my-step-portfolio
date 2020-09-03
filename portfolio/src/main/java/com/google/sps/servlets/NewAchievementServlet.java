4eea8ac295c00e78e48d7c4fd16b42e963ef0c4d
omermadmon <omermadmon@gmail.com>
2020-09-03 10:06:58 +0000

    altered new achievement servlet to add serial number.

--- portfolio/src/main/java/com/google/sps/servlets/NewAchievementServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/NewAchievementServlet.java
@@ -17,6 +17,10 @@ package com.google.sps.servlets;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.SortDirection;
+import com.google.appengine.api.datastore.Query.FilterPredicate;
+import com.google.appengine.api.datastore.Query.FilterOperator;
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -29,13 +33,22 @@ public class NewAchievementServlet extends HttpServlet {
 
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+    // Count existing achievement.
+    Query query = new Query("Achievement");
+
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    PreparedQuery results = datastore.prepare(query);
     
     // Create new achievement entity.
     Entity achievementEntity = new Entity("Achievement");
+    int serial = results.countEntities();
+    String achievementText = request.getParameter("text") + "\nSerial number: " + serial;
     achievementEntity.setProperty("title", "Maccabi Haifa FC");
     achievementEntity.setProperty("club", "mhfc");
     achievementEntity.setProperty("geo", "{ \"latitude\": 32.794, \"longitude\": 34.9896 }");
-    achievementEntity.setProperty("text", request.getParameter("text"));
+    achievementEntity.setProperty("text", achievementText);
+    achievementEntity.setProperty("serial", serial);
 
     // Add achievement to datastore.
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
